ddc0e8b15a89629566349334cd7b9cead232d344
mxiaonian <41322284+mxiaonian@users.noreply.github.com>
2024-07-29 16:00:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import threading
 import tkinter as tk
 from tkinter import filedialog, ttk
 from tkinter import messagebox
+
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 PASSWORD_FILE = 'passwords.json'
@@ -21,7 +22,10 @@ def handle_output(stream, log_text_widget, stream_name):
 
 
 def build_command(archive_path, extract_path, password=None):
-    base_command = ['7z', 'x', archive_path, f'-o{extract_path}']
+    # 获取当前脚本目录
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    seven_zip_path = os.path.join(script_dir, '7z.exe')
+    base_command = [seven_zip_path, 'x', archive_path, f'-o{extract_path}']
     if password:
         # 通过列表解析避免直接字符串拼接，减少命令注入风险
         password_option = ['-p' + password]
@@ -76,7 +80,7 @@ def extract_all(input_path, output_path, password=None, log_text_widget=None):
         os.makedirs(output_path)
 
     extracted_files = set()  # 用于存储已解压文件的集合
-    dirs_to_process = queue.Queue()   # 创建一个队列，用于存储待处理的目录
+    dirs_to_process = queue.Queue()  # 创建一个队列，用于存储待处理的目录
     for root, dirs, files in os.walk(input_path):  # 遍历输入目录中的所有文件和子目录
         for file in files:
             file_path = os.path.join(root, file)  # 获取当前文件的完整路径
@@ -93,7 +97,7 @@ def extract_all(input_path, output_path, password=None, log_text_widget=None):
                         log_text_widget.insert(tk.END, f"已解压文件: {result}\n")  # 在日志中记录解压的文件
                         extracted_files.add(do_file)  # 将解压过的文件添加到集合中
                         # 遍历第一次解压路径下的所有压缩包
-                        for root, dirs, files in os.walk(output_path):   # 遍历解压路径下的所有文件
+                        for root, dirs, files in os.walk(output_path):  # 遍历解压路径下的所有文件
                             for file in files:
                                 output_file_path = os.path.join(root, file)
                                 if is_compressed_or_no_extension(output_file_path):
@@ -150,21 +154,11 @@ def select_output_directory():
     output_dir_var.set(path)
 
 
-# 选择zip路径
-def select_zip_directory():
-    path = filedialog.askdirectory()
-    zip_dir_var.set(path)
-
-
 # 开始解压
 def start_extraction():
     input_path = input_dir_var.get()
     output_path = output_dir_var.get()
     password = password_var.get()
-
-    if not find_7zip_path():
-        messagebox.showerror("错误", "未找到7-Zip程序，请手动输入7-Zip程序路径")
-        return
 
     if not input_path or not output_path:
         messagebox.showwarning("警告", "请填写所有必要的字段。")
@@ -177,28 +171,6 @@ def start_extraction():
 
     extraction_thread = threading.Thread(target=extract_all, args=(input_path, output_path, password, log_text))
     extraction_thread.start()
-
-
-def find_7zip_path():
-    # 常见的7-Zip安装位置
-    common_paths = [
-        r"C:\Program Files\7-Zip\7z.exe",
-        r"C:\Program Files (x86)\7-Zip\7z.exe"
-    ]
-
-    # 检查每个可能的位置
-    for path in common_paths:
-        if os.path.isfile(path):
-            return path
-
-    # 如果没有找到，尝试从系统环境变量中查找
-    for path in os.environ['PATH'].split(os.pathsep):
-        exe_file = os.path.join(path.strip('"'), '7z.exe')
-        if os.path.isfile(exe_file):
-            return exe_file
-
-    # 如果所有方法都失败了，返回False
-    return False
 
 
 # 加载密码
@@ -223,13 +195,6 @@ def save_new_password(password):
         password_combobox['values'] = passwords
 
 
-# 处理拖放事件
-def on_drop_7zip(event):
-    # 移除大括号
-    cleaned_data = event.data.replace('{', '').replace('}', '')
-    zip_dir_var.set(cleaned_data)
-
-
 def on_drop(event):
     # 移除大括号
     cleaned_data = event.data.replace('{', '').replace('}', '')
@@ -245,18 +210,6 @@ def on_drop_2(event):
 # 创建主窗口
 root = TkinterDnD.Tk()
 root.title("套娃解压工具")
-
-# 解压软件路径
-tk.Label(root, text="7zip路径:").grid(row=0, column=0, padx=10, pady=10)
-zip_dir_var = tk.StringVar()
-zip_path = zip_dir_var.get()
-if not zip_path:
-    zip_path = [r"C:\Program Files\7-Zip\7z.exe"]
-zip_entry = tk.Entry(root, textvariable=zip_dir_var, width=50)
-zip_entry.grid(row=0, column=1, padx=10, pady=10)
-tk.Button(root, text="选择", command=select_zip_directory).grid(row=0, column=2, padx=10, pady=10)
-zip_entry.drop_target_register(DND_FILES)
-zip_entry.dnd_bind('<<Drop>>', on_drop_7zip)
 
 # 输入目录
 tk.Label(root, text="输入目录:").grid(row=1, column=0, padx=10, pady=10)
